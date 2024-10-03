3dfffe97888f1bf4d5381eb2c1e16b3e409a9b55
alvaro547 <a.23308051281175@cbtis128.edu.mx>
2024-10-03 15:51:41 -0600

    Add files via upload

---
+++ Pr1_2nda unidad_precios mayor-menor/Pr1_2nda unidad_precios mayor-menor.py
@@ -0,0 +1,13 @@
+print(" ")
+print("Alvaro Campéchano Estrada 3W")
+print(" ")
+# Almacenar los precios en una lista
+precios = [43, 57, 92, 20, 37, 5, 9]
+
+# Encontrar el menor y el mayor precio
+menor_precio = min(precios)
+mayor_precio = max(precios)
+
+# Mostrar los resultados
+print(f"El menor precio es: {menor_precio}")
+print(f"El mayor precio es: {mayor_precio}")
